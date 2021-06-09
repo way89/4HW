d121c78573a81f9eb583c4769d2234b6b8b6b749
way89 <74764319+way89@users.noreply.github.com>
2021-06-09 18:49:35 +0800

    Update DropdownListShow.aspx.cs

--- 4HW/DropdownListShow.aspx.cs
+++ 4HW/DropdownListShow.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace _4HW
 {
-    public partial class DropdownListShow : System.Web.UI.Page
+    public partial class DropdownListShow : System.Web.UI.Page 
     { 
         protected void Page_Load(object sender, EventArgs e)
         {
